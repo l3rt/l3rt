cbde4ad18729f725c442c28025ee3c32f6578b7d
Grigoriy Sterin <gr.sterin@gmail.com>
2017-10-10 18:59:37 -0400

    Issue #4. Ability to override config parameters for targets.

--- core/src/main/scala/lert/core/rule/target/HipChatTarget.scala
+++ core/src/main/scala/lert/core/rule/target/HipChatTarget.scala
@@ -1,15 +1,11 @@
 package lert.core.rule.target
 
-import javax.inject.Inject
-
-import lert.core.config.ConfigProvider
+import lert.core.config.Config
 import io.evanwong.oss.hipchat.v2.HipChatClient
 import io.evanwong.oss.hipchat.v2.rooms.MessageColor
 
-class HipChatTarget @Inject()(configProvider: ConfigProvider) {
-  def send(room: String, message: String, color: String, notify: Boolean) = {
-    val config = configProvider.config
-
+class HipChatTarget {
+  def send(room: String, message: String, color: String, notify: Boolean)(implicit config: Config) = {
     val client = new HipChatClient(config.targetSettings.hipchat.accessToken)
 
     val builder = client.prepareSendRoomNotificationRequestBuilder(room, message)
